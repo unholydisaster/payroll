c25f075cb37179e1b9f8919ef78f0643b45ef036
Your Name <unholydisaster>
2022-05-09 23:31:58 +0300

    changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Employed } from "../employees"
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const {method}=req;
+if(method==="GET"){
+    return res.status(200).json(Employed)
+}
+
+if(method === "POST"){
+  const {body}=req;
+  Employed.push({ ...body, id:Employed.length +1});
+  return res.status(200).json(Employed);
+}
 }
---
+++ pages/employees.js
@@ -0,0 +1,64 @@
+
+export const Employed=[{
+  Fullname:"Kevin Kimanthi",
+  Nationalid:39403458,
+  Hoursworked:40,
+  Basicpay:18,
+  Houseallowance:50,
+  Conveyanceallowance:40,
+  Childreneducationallowance:50,
+  FuelReimbursements:50,
+  DriverReimbursements:100,
+  HealthInsurance:10,
+  Taxincome:10
+},
+{
+  Fullname:"Dennis Kimeu",
+  Nationalid:39456782,
+  Hoursworked:45,
+  Basicpay:25,
+  Houseallowance:50,
+  Conveyanceallowance:40,
+  Childreneducationallowance:50,
+  FuelReimbursements:50,
+  DriverReimbursements:100,
+  HealthInsurance:10,
+  Taxincome:10
+},
+{
+  Fullname:"Gideon Abangi",
+  Nationalid:37594213,
+  Hoursworked:45,
+  Basicpay:30,
+  Houseallowance:50,
+  Conveyanceallowance:40,
+  Childreneducationallowance:50,
+  FuelReimbursements:50,
+  DriverReimbursements:0,
+  HealthInsurance:10,
+  Taxincome:10
+},
+{
+  Fullname:"Jack mahui",
+  Nationalid:37804215,
+  Hoursworked:45,
+  Basicpay:25,
+  Houseallowance:50,
+  Conveyanceallowance:40,
+  Childreneducationallowance:50,
+  FuelReimbursements:50,
+  DriverReimbursements:100,
+  HealthInsurance:10,
+  Taxincome:10
+}]
+
+const Employers=()=>{
+
+  return(
+    <div>
+      <h1>data is stored here</h1>
+    </div>
+  )
+}
+
+export default Employers
